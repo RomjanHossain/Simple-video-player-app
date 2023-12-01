1a557b446a6f7701eb8b5ca8f645d9dad8e72dcc
RomjanHossain <romjanhossain726526@gmail.com>
2023-12-01 22:50:25 +0600

    boilarplatee...........

--- lib/presentation/pages/homepage/widgets/homepage_body.dart
+++ lib/presentation/pages/homepage/widgets/homepage_body.dart
@@ -6,15 +6,29 @@ import 'package:simple_video_player_app/presentation/pages/homepage/bloc/bloc.da
 ///
 /// Add what it does
 /// {@endtemplate}
-class HomepageBody extends StatelessWidget {
+class HomepageBody extends StatefulWidget {
   /// {@macro homepage_body}
   const HomepageBody({super.key});
 
   @override
+  State<HomepageBody> createState() => _HomepageBodyState();
+}
+
+class _HomepageBodyState extends State<HomepageBody> {
+  @override
   Widget build(BuildContext context) {
     return BlocBuilder<HomepageBloc, HomepageState>(
       builder: (context, state) {
-        return Center(child: Text(state.customProperty));
+        return Column(
+          children: [
+            Text(
+              'Trending Videos',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+          ],
+        );
       },
     );
   }
