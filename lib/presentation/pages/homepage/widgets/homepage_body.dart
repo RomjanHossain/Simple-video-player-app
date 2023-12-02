1aa5752a62bfd328208e7183230c3d3b1c1e0944
RomjanHossain <romjan@skiff.com>
2023-12-02 12:19:28 +0600

    goto viewvideo page on click

--- lib/presentation/pages/homepage/widgets/homepage_body.dart
+++ lib/presentation/pages/homepage/widgets/homepage_body.dart
@@ -1,10 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:simple_video_player_app/data/datasources/remote/api_data.dart';
 import 'package:simple_video_player_app/domain/entities/video_response_entity.dart';
 import 'package:simple_video_player_app/domain/entities/video_response_result_entity.dart';
+import 'package:simple_video_player_app/presentation/cubit/current_video_cubit.dart';
 import 'package:simple_video_player_app/presentation/pages/homepage/bloc/bloc.dart';
 import 'package:simple_video_player_app/presentation/pages/homepage/components/thumbnail_card.dart';
+import 'package:simple_video_player_app/presentation/pages/view_video/view/view_video_page.dart';
 
 /// {@template homepage_body}
 /// Body of the HomepagePage.
@@ -75,8 +78,15 @@ class _HomepageBodyState extends State<HomepageBody> {
                   pagingController: _pagingController,
                   builderDelegate:
                       PagedChildBuilderDelegate<VideoResponseResultEntity>(
-                    itemBuilder: (context, item, index) => VideoThumbnailCard(
-                      videoResponseResultEntity: item,
+                    itemBuilder: (context, item, index) => GestureDetector(
+                      onTap: () {
+                        BlocProvider.of<CurrentVideoCubit>(context)
+                            .setVideo(item);
+                        Navigator.push(context, ViewVideoPage.route());
+                      },
+                      child: VideoThumbnailCard(
+                        videoResponseResultEntity: item,
+                      ),
                     ),
                   ),
                 ),
